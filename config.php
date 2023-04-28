4989b58dd6311983738d40a06081990a1f66c557
Samir <ahmedsamir25999@gmail.com>
2023-04-28 18:54:53 +0300

    fixing bugs in last visit

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define("HOST","localhost");
 define("USER","root");
 define("PASS","");
-define("DB","articles_system");
+define("DB","articles");
 define("RECORDS_PER_PAGE",10);
 define("Debug__Mode",0);
 
--- views/users/welcome.php
+++ views/users/welcome.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+require_once '../../vendor/autoload.php'; 
 // check if last visit is stored in session
 // if (isset($_SESSION['last_visit'])) {
 //     $lastVisit = $_SESSION['last_visit'];
@@ -13,6 +13,30 @@ session_start();
 // $sql="INSERT INTO users (last_visit) VALUES ('$login_time') where id=$_SESSION['user']['id']";
 // set current time as last visit
 // $_SESSION['last_visit'] = date('F j, Y, g:i a');
+
+
+
+$db = new MySqlHandler("users");
+$login_time = date('F j, Y, g:i a');
+$user_id = $_SESSION['user']['id'];
+
+// select last visit
+$sql1="SELECT last_visit FROM users WHERE id='$user_id'";
+$ArrayOFLastVist = $db->getResults($sql1);
+$last_visit = $ArrayOFLastVist[0]['last_visit'];
+if($last_visit){
+    echo "Welcome back! Your last visit was on $last_visit";
+}
+else {
+    echo "Welcome to our website!";
+}
+
+$sql2 = "UPDATE users SET last_visit = '$login_time' WHERE id = '$user_id'";
+$result= $db->executeQuery($sql2);
+
+
+
+
 ?>
 
 <!DOCTYPE html>
