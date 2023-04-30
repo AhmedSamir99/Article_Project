b9a5fbb8cfebc50ded20fe03c3d2a28d3a186d49
Raneen Mahmoud <raneenm260@gmail.com>
2023-04-30 21:42:47 +0200

    edit for delete image for article

--- views/Articles/delete_article.php
+++ views/Articles/delete_article.php
@@ -6,9 +6,8 @@ $id = (isset($_GET["id"]))? intval($_GET["id"])  : 0;
     $sql="SELECT image FROM articles WHERE id=".$id;
     $result= $db->getResults($sql);
 
-    $file_path="../../images/".$result[0]['image'];
-    unlink($file_path); // delete the image file
-
+    // $file_path="../../images/".$result[0]['image'];
+    // unlink($file_path); // delete the image file
     $db->delete($id); // delete the row from the database
 ?>
 <script>
